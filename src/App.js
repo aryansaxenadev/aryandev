432083437bfa611faaa4401c7b25feb3ccbf8ad5
AryanSaxena7707 <aryan07mernstack@gmail.com>
2024-07-01 15:51:51 +0100

    proxy

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import Navbar from './components/NavBar';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Projects from './components/Projects';
+import ContactMe from './components/Contact';
+import Divider from './components/Divider';
+import { Element } from 'react-scroll';
+import LoadingSpinner from './components/Loading';
+import ContactForm from './components/ContactForm';
+import chatIcon from './chat.png'; // Add correct path for chat icon
+import closeIcon from './close-icon.png'; // Add correct path for close icon
+import ElevateYourWebPresent from './components/ElevateYourWebPresent';
+import Blog from './components/Blog'; // Add correct path for Blog component
+import BlogDetail from './components/BlogDetail'; // Add correct path for BlogDetail component
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [isFormVisible, setIsFormVisible] = useState(false);
+  const location = useLocation();
+
+  const toggleFormVisibility = () => {
+    setIsFormVisible(!isFormVisible);
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await new Promise(resolve => setTimeout(resolve, 780));
+      setLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+  const showNavbar = !location.pathname.startsWith('/blog');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {loading && <LoadingSpinner />}
+      {!loading && (
+        <>
+          {showNavbar && <Navbar />}
+          <ContactForm 
+            isFormVisible={isFormVisible} 
+            toggleFormVisibility={toggleFormVisibility} 
+            chatIcon={chatIcon} 
+            closeIcon={closeIcon} 
+          />
+          <Routes>
+            <Route path="/" element={
+              <>
+                <Element name="/">
+                  <Home toggleFormVisibility={toggleFormVisibility} />
+                </Element>
+                <Element name="about">
+                  <About />
+                </Element>
+                <Element name="skills">
+                  <Skills />
+                </Element>
+                <Element name="divider">
+                  <Divider />
+                </Element>
+                <Element name="projects">
+                  <Projects />
+                  <Divider />
+                </Element>
+                <ElevateYourWebPresent />
+                <Element name="contact">
+                  <ContactMe />
+                </Element>
+              </>
+            } />
+            <Route path="/blog" element={<Blog />} />
+            <Route path="/blog/:id" element={<BlogDetail />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </>
+      )}
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { Link as ScrollLink } from 'react-scroll';
+import './NavBar.css';
+import logo from '../logo2.png';
+import { FaBars } from 'react-icons/fa'; // Import hamburger icon from react-icons library
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleBlogClick = () => {
+    window.location.href = '/blog';
+  };
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-logo">
+          <img src={logo} alt="Logo" />
+          <span>AryanDev</span>
+        </div>
+        <div className="menu-icon" onClick={toggleMenu}>
+          <FaBars /> {/* Hamburger icon */}
+        </div>
+        <ul className={isOpen ? 'nav-menu active' : 'nav-menu'}>
+          <li className="nav-item">
+            <ScrollLink to="about" smooth={true} duration={500} offset={-50} onClick={toggleMenu}>About Me</ScrollLink>
+          </li>
+          <li className="nav-item">
+            <ScrollLink to="skills" smooth={true} duration={500} offset={-50} onClick={toggleMenu}>Skills</ScrollLink>
+          </li>
+          <li className="nav-item">
+            <ScrollLink to="divider" smooth={true} duration={500} offset={-50} onClick={toggleMenu}>Projects</ScrollLink>
+          </li>
+          <li className="nav-item">
+            <ScrollLink to="contact" smooth={true} duration={500} offset={-50} onClick={toggleMenu} className="contact-button">Contact Me</ScrollLink>
+          </li>
+          <li className="nav-item">
+            <a href="aryandev/blog" onClick={handleBlogClick}>My Blogs</a>
+          </li>
+        </ul>
+        <div className="nav-social-icons">
+          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"></a>
+          <a href="https://github.com" target="_blank" rel="noopener noreferrer"></a>
+          <a href="https://behance.net" target="_blank" rel="noopener noreferrer"></a>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
